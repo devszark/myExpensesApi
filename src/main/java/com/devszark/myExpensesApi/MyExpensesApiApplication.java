93c1f24d88b869e5acba695accbba205ae780622
devszark <devszark@gmail.com>
2023-04-23 19:56:54 +0200

    First commit

---
+++ src/main/java/com/devszark/myExpensesApi/MyExpensesApiApplication.java
@@ -0,0 +1,13 @@
+package com.devszark.myExpensesApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpensesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExpensesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devszark/myExpensesApi/MyExpensesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devszark.myExpensesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExpensesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
